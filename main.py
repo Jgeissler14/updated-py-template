74b6d3c1127ff1b28c106192313c5d9e140c8a95
Jgeissler14 <66270580+Jgeissler14@users.noreply.github.com>
2021-05-26 13:14:10 -0400

    First commit

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import glob
 import json
 import os
 import traceback
+import boto3
+from re import search
+import re
+import logging
+from botocore.exceptions import ClientError
+from s3_upload import upload_file
+
 
 # Directory variables (root dir, data dir, etc.)
 import pandas
 
-DATA_DIR = os.path.join(os.getcwd(), 'data')
-OUTPUT_DIR = os.path.join(os.getcwd(), 'output')
-INPUT_DIR = os.path.join(os.getcwd(), 'input')
 
+DIR = os.getcwd()
+
+#Path to input file(s)
+file_list = ["/Users/joshua.geissler/Documents/GitHub/updated-py-template/sample_1.csv", 
+"/Users/joshua.geissler/Downloads/sample_4.csv", 
+"https://discover-s3-testing.s3.amazonaws.com/sample_2.csv", 
+"https://discover-s3-testing.s3.amazonaws.com/random/sample_3.csv"]
 
 def main():
     # This script will be executed in a container in a batch environment.
@@ -25,7 +36,7 @@ def main():
     message = os.getenv("AD_MESSAGE", "Running a Sample Python Script on Discover")
     print(message)
 
-    df = pandas.read_csv(os.path.join(DATA_DIR, 'icd10cm_codes_2020.csv'))
+    df = pandas.read_csv(os.path.join(os.getcwd(), 'icd10cm_codes_2020.csv'))
 
     # Discover UI uses 'results.json' file to display the output to use
     # For information on results.json format see: ???
@@ -34,7 +45,35 @@ def main():
     # Results object
     results_dict = {}
 
-    for input_file in glob.glob(os.path.join(INPUT_DIR, "*.csv")):
+    for input_file in file_list:
+        #boolean variable to track if input_file is local
+        local = True
+        if search("s3.amazonaws.com", input_file):
+            local = False
+            #Use Regex to find the bucket and key matches
+            Regex = re.search('https://(.+?).s3.amazonaws.com/(.*)', input_file)
+
+            #Set Bucket Name
+            bucket_name = Regex.group(1)
+            #print("Bucket : " + bucket_name)
+            
+            #Set Object Key
+            key = Regex.group(2)
+            #print("Key : " + key)
+
+            #Initialize S3 and set bucket
+            s3 = boto3.resource('s3')
+            my_bucket = s3.Bucket(bucket_name)
+            path, filename = os.path.split(key)
+            my_bucket.download_file(key, filename)
+
+            DIR = os.getcwd()
+            input_file = os.path.join(os.getcwd(), filename)
+
+        else:
+            DIR = os.path.dirname(input_file)
+
+        #print("DIRECTORY: " + DIR)
         df_in = pandas.read_csv(input_file, header=None, names=['code'])
 
         for c in df_in['code']:
@@ -48,13 +87,13 @@ def main():
         results_df = pandas.DataFrame(results_dict).transpose()
 
         # Save results file to csv file
-        print(f'Saving results to {OUTPUT_DIR} directory ...')
+        print(f'Saving results to {DIR} directory ...')
 
         # extract the input filename for generating related result files
         input_filename = os.path.splitext(os.path.basename(input_file))[0]
 
         output_filename = f'{input_filename}_result.csv'
-        results_df.to_csv(os.path.join(OUTPUT_DIR, output_filename), header=False)
+        results_df.to_csv(os.path.join(DIR, output_filename), header=False)
 
         # Note: Use filename to display the content of a file, and link to generate a link to download results
         output_results["data"].append({
@@ -63,7 +102,14 @@ def main():
             "link": output_filename,
         })
 
-    with open(os.path.join(OUTPUT_DIR, "results.json"), "w+") as f:
+        #Save Results back to S3 if needed
+        if not local:
+            if path:
+                upload_file(input_file, bucket_name, path + "/" + output_filename)
+            else:
+                upload_file(input_file, bucket_name, output_filename)
+
+    with open(os.path.join(DIR, "results.json"), "w+") as f:
         print("Writing results.json file")
         json.dump(output_results, f)
         f.close()
@@ -75,7 +121,7 @@ if __name__ == '__main__':
     except Exception as e:
         tb = traceback.format_exc()
 
-        with open(os.path.join(OUTPUT_DIR, "results.json"), "w+") as f:
+        with open(os.path.join(os.getcwd(), "results.json"), "w+") as f:
             print("Writing errors in results.json file")
             json.dump({
                 "data_type": "generated",
